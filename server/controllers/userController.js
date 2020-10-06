2b2a1f41d19be0743547385a271e4b301a1bea9e
Umma Ahimsha <uahimsha@gmail.com>
2020-10-06 11:58:56 +0700

    add back-end user auth

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -24,4 +24,56 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+// desc: Register a new user
+// desc: POST /api/users
+// access: Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exist");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
+
+// desc: Get user profile
+// desc: GET /api/users/profile
+// access: Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("Invalid email or passowrd");
+  }
+});
+
+export { authUser, getUserProfile, registerUser };
