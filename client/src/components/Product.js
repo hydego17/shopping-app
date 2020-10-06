dfdab774ef9a98ffff0c7a7eab3505778fd3dc6b
Umma Ahimsha <uahimsha@gmail.com>
2020-10-06 12:22:18 +0700

    Added user auth

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -2,29 +2,28 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Card } from "react-bootstrap";
 import Rating from "./Rating";
-import { motion } from "framer-motion";
 
 const Product = ({ product }) => {
   return (
-      <Card className="card my-3 rounded">
+    <Card className="card my-3 rounded">
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </Link>
+      <Card.Body>
         <Link to={`/product/${product._id}`}>
-          <Card.Img src={product.image} variant="top" />
+          <Card.Title as="div">
+            <strong className="product-name">{product.name}</strong>
+          </Card.Title>
         </Link>
-        <Card.Body>
-          <Link to={`/product/${product._id}`}>
-            <Card.Title as="div">
-              <strong className="product-name">{product.name}</strong>
-            </Card.Title>
-          </Link>
-          <Card.Text as="div">
-            <Rating
-              value={product.rating}
-              // text={`${product.numReviews} reviews`}
-            />
-          </Card.Text>
-          <Card.Text as="h5">IDR {product.price}</Card.Text>
-        </Card.Body>
-      </Card>
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            // text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
+        <Card.Text as="h5">IDR {product.price}</Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
